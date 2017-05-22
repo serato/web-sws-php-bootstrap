c8985169af8963a33ffe329a87cb12acfd165d44
Chris Deighton <chris@serato.com>
2017-05-22 22:32:07 +1200

    Add CsvFileIterator class

---
+++ src/Utils/CsvFileIterator.php
@@ -0,0 +1,74 @@
+<?php
+namespace Serato\SwsApp\Utils;
+
+use Exception;
+
+/**
+ * Provides an iterable interface to a CSV file.
+ *
+ * @link http://php.net/manual/en/class.iterator.php
+ */
+class CsvFileIterator implements \Iterator
+{
+    private $file;
+    private $key = 0;
+    private $current;
+    private $valid = false;
+
+    /**
+     * Constructs the class
+     *
+     * @param string $filePath  Path to CSV file
+     */
+    public function __construct(string $filePath)
+    {
+        if (!file_exists($filePath)) {
+            throw new Exception("File '$filePath' not found");
+        }
+        $this->file = fopen($filePath, 'r');
+        $this->readLine();
+    }
+
+    public function __destruct()
+    {
+        fclose($this->file);
+    }
+
+    public function rewind()
+    {
+        rewind($this->file);
+        $this->readLine();
+        $this->key = 0;
+    }
+
+    public function valid()
+    {
+        return $this->valid;
+    }
+
+    public function key()
+    {
+        return $this->key;
+    }
+
+    public function current()
+    {
+        return $this->current;
+    }
+
+    public function next()
+    {
+        $this->readLine();
+        $this->key++;
+    }
+
+    private function readLine()
+    {
+        if (feof($this->file)) {
+            $this->valid = false;
+        } else {
+            $this->valid = true;
+            $this->current = fgetcsv($this->file);
+        }
+    }
+}
---
+++ tests/Utils/CsvFileIteratorTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Serato\SwsApp\Test\Utils;
+
+use Serato\SwsApp\Test\TestCase;
+use Serato\SwsApp\Utils\CsvFileIterator;
+
+/**
+ * Unit tests for Serato\SwsApp\Utils\CsvFileIterator
+ */
+class CsvFileIteratorTest extends TestCase
+{
+    /**
+     * @expectedException \Exception
+     */
+    public function testFileNotFound()
+    {
+        $csv = new CsvFileIterator(__DIR__ . '/../resources/nosuchfile.csv');
+        $this->assertTrue(true);
+    }
+
+    public function testSmokeTest()
+    {
+        $csv = new CsvFileIterator(__DIR__ . '/../resources/csv.csv');
+        $i = 0;
+        foreach ($csv as $data) {
+            $this->assertEquals(count($data), 5);
+            $i++;
+        }
+        $this->assertEquals($i, 5);
+    }
+
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testIterator($a, $b, $c, $d, $e)
+    {
+        $this->assertEquals(($a + $b + $c + $d + $e), 15);
+    }
+
+    public function dataProvider()
+    {
+        return new CsvFileIterator(__DIR__ . '/../resources/csv.csv');
+    }
+
+}
