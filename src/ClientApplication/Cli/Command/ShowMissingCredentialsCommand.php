3c2df7fa1ab66511f302e5d5ff8028434ccb6b7f
Chris Deighton <chris@serato.com>
2018-12-04 14:26:03 +1300

    WEB-2469
    Skeleton CLI application and commands

---
+++ src/ClientApplication/Cli/Command/ShowMissingCredentialsCommand.php
@@ -0,0 +1,37 @@
+<?php
+namespace Serato\SwsApp\ClientApplication\Cli\Command;
+
+use Serato\SwsApp\ClientApplication\Cli\Command\AbstractCommand;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ShowMissingCredentialsCommand extends AbstractCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        parent::configure();
+        $this
+            ->setName('show-missing-credentials')
+            ->setDescription('Shows missing credentials')
+            ->setHelp(
+                "Shows missing credentials.\n\n" .
+                "Defaults to displaying missing credentials for all applications in the.\n" .
+                "environment Can display missing credentials for a single application by\n" .
+                "using the --" . self::OPTION_APP_NAME . " option.\n\n" .
+                "Defaults to using the current runtime environment. This can be overridden with\n" .
+                "the --" . self::OPTION_ENVIRONMENT . " argument.\n"
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //
+    }
+}
