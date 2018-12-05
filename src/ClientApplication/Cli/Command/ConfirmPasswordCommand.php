6bda56ad2937ff92d72082b83b1cf46278849ebf
Chris Deighton <chris@serato.com>
2018-12-05 14:29:59 +1300

    WEB-2469
    Adds implementation for the `confirm-password` command

--- src/ClientApplication/Cli/Command/ConfirmPasswordCommand.php
+++ src/ClientApplication/Cli/Command/ConfirmPasswordCommand.php
@@ -5,6 +5,9 @@ use Serato\SwsApp\ClientApplication\Cli\Command\AbstractCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+use Exception;
 
 class ConfirmPasswordCommand extends AbstractCommand
 {
@@ -17,13 +20,11 @@ class ConfirmPasswordCommand extends AbstractCommand
         $this
             ->setName('confirm-password')
             ->addArgument(self::ARG_APP_ID, InputArgument::REQUIRED, 'Application ID')
-            ->addArgument(self::ARG_PASSWORD, InputArgument::REQUIRED, 'Application Password')
             ->setDescription('Confirms a password for a given application')
             ->setHelp(
                 "Confirms a password for an applicaton.\n\n" .
-                "Uses the provided <" . self::ARG_APP_ID . "> and <" . self::ARG_PASSWORD .
-                "> arguments to confirm the the client\n" .
-                "application data contains the correct password hash.\n" .
+                "Prompts the user to provide a password and confirms that it matches with the\n" .
+                "password hash stored against <" . self::ARG_APP_ID . ">.\n" .
                 $this->getCommonHelpText()
             );
     }
@@ -34,5 +35,51 @@ class ConfirmPasswordCommand extends AbstractCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         parent::execute($input, $output);
+
+        $appId = $input->getArgument(self::ARG_APP_ID);
+
+        $data = $this->getDataLoader()->getApp($this->getEnv(), $this->getUseCache());
+
+        $passwordHash = $this->getPasswordHash($data, $appId);
+
+        $helper = $this->getHelper('question');
+
+        $question = new Question("Password for app '$appId'?");
+        $question->setHidden(true);
+        $question->setHiddenFallback(false);
+
+        $password = $helper->ask($input, $output, $question);
+
+        $output->getFormatter()->setStyle(
+            'yes',
+            new OutputFormatterStyle('green', 'black', ['bold'])
+        );
+        $output->getFormatter()->setStyle(
+            'no',
+            new OutputFormatterStyle('red', 'black', ['bold'])
+        );
+
+        $this->writeInfoHeader($output, 'Confirm Password', ['Application ID' => $appId]);
+        $output->writeln(
+            "\nPassword match? " .
+            (password_verify($password, $passwordHash) ? "<yes>Yes</yes>" : "<no>No</no>") .
+            "\n"
+        );
+    }
+
+    private function getPasswordHash(array $appData, string $appId): string
+    {
+        foreach ($appData as $name => $data) {
+            if ($data['id'] === $appId) {
+                if (!isset($data['password_hash'])) {
+                    throw new Exception(
+                        "No password hash found for app ID '$appId' in '"  . $this->getEnv(). "' environment"
+                    );
+                } else {
+                    return $data['password_hash'];
+                }
+            }
+        }
+        throw new Exception("Application ID '$appId' not found in '"  . $this->getEnv(). "' environment");
     }
 }
