9f0736373f7641b22c7803d9a42b4a0ad6aff41f
Mehdi Shahidi <mehdi.shahidi@serato.com>
2019-04-29 15:14:59 +1200

    Customize Monolog JsonFormatter to make it clouwatch logs frirendly

---
+++ src/Utils/MonologJsonFormatter.php
@@ -0,0 +1,36 @@
+<?php
+namespace Serato\SwsApp\Utils;
+
+use Monolog\Formatter\JsonFormatter;
+use \DateTime;
+
+class MonologJsonFormatter extends JsonFormatter
+{
+    const SIMPLE_DATE = "Y-m-d H:i:s";
+
+    protected $dateFormat;
+    /**
+     * @param int $batchMode
+     * @param bool $appendNewline
+     */
+    public function __construct(
+        int $batchMode = parent::BATCH_MODE_JSON,
+        bool $appendNewline = true,
+        string $dateFormat = self::SIMPLE_DATE
+    ) {
+        $this->dateFormat = $dateFormat;
+        parent::__construct($batchMode, $appendNewline);
+    }
+
+    /**
+     * Formats the record and adds the Date to it
+     */
+    public function format(array $record): string
+    {
+        $dateTime = '';
+        if ($record['datetime'] !== null && $record['datetime'] instanceof DateTime) {
+            $dateTime = $record['datetime']->format($this->dateFormat);
+        }
+        return "[$dateTime] " . parent::format($record);
+    }
+}
