6651daac55fb2d46aa37ff070fd5b766a36f8327
MikeS <mike.sabharwal@serato.com>
2020-08-24 18:07:46 +1200

    [WEB-4594] New exception type for invalid payload data

---
+++ src/Http/Rest/Exception/AbstractUnsupportedMediaTypeException.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Serato\SwsApp\Http\Rest\Exception;
+
+use Serato\SwsApp\Http\Rest\Exception\AbstractException;
+
+/**
+ * Abstract Unsupported Media Type Exception
+ *
+ * This exception is thrown when the server refuses to accept the request because the payload format is in an
+ * unsupported format.
+ *
+ * The format problem might be due to the request's indicated Content-Type or Content-Encoding, or as a result of
+ * inspecting the data directly.
+ */
+
+abstract class AbstractUnsupportedMediaTypeException extends AbstractException
+{
+    /**
+     * The HTTP response code associated with the client error.
+     *
+     * @var int
+     */
+    protected $http_response_code = 415;
+}
