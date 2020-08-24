300c0133731b8216e063b7bb75401cedfd1681d0
MikeS <mike.sabharwal@serato.com>
2020-08-25 11:28:53 +1200

    [WEB-4594] Make the exception a bit more specific.

---
+++ src/Http/Rest/Exception/UnsupportedContentTypeException.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Serato\SwsApp\Http\Rest\Exception;
+
+use Serato\SwsApp\Http\Rest\Exception\AbstractException;
+
+/**
+ * Unsupported Content-Type Exception
+ *
+ * This exception is thrown when the server refuses to accept the request because the payload format is in an
+ * unsupported format.
+ *
+ * The format problem would be due to the request's indicated Content-Type.
+ */
+
+class UnsupportedContentTypeException extends AbstractException
+{
+    protected $http_response_code = 415;
+    protected $code = 2003;
+    protected $message = 'Invalid request Content-Type. Refer to the API spec for more details.';
+}
